9eb675c957572ce63a70451f10e8061e13de5443
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2019-09-24 19:22:58 +0000

    deploy

---
+++ precache-manifest.bf969bc06a04eff6ce11d25804845106.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c75e834088dd1cc4dc927f0b6113a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c1da8f65c8ac0f4a14",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "96be9e5c51b3e19d1478",
+    "url": "/static/css/main.8ac15f3f.chunk.css"
+  },
+  {
+    "revision": "e9c1da8f65c8ac0f4a14",
+    "url": "/static/js/2.dde692d4.chunk.js"
+  },
+  {
+    "revision": "96be9e5c51b3e19d1478",
+    "url": "/static/js/main.a02c04de.chunk.js"
+  },
+  {
+    "revision": "08c80a191cef3a124f37",
+    "url": "/static/js/runtime~main.c91b1172.js"
+  },
+  {
+    "revision": "774fc43ed621ad6f92b46fda9581059e",
+    "url": "/static/media/logo.774fc43e.png"
+  }
+]);
